fc797cde524ad482f5c9959d5ae089bd0925837d
Mateus Henrique <mateushenriquechaves1@gmail.com>
2021-08-10 04:23:12 -0300

    code base

---
+++ src/test/java/alura/com/forum/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.com.forum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
